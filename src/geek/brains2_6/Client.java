e225638ee9a604283c0e750990729dec2bb3c946
Sasha Korn <you@example.com>
2020-04-01 20:51:28 +0300

    Сервер-клиент

--- src/geek/brains2_6/Client.java
+++ src/geek/brains2_6/Client.java
@@ -1,4 +1,59 @@
 package geek.brains2_6;
 
-public class CLient {
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public Client(Socket socket) throws IOException {
+        inputStream = new DataInputStream(socket.getInputStream());
+        outputStream = new DataOutputStream(socket.getOutputStream());
+    }
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 8189)) {
+            System.out.println("Клиент запущен.");
+            Client client = new Client(socket);
+            Thread getMessage = new Thread(client::messageGet);
+            Thread sendMessage = new Thread(client::messageSend);
+            sendMessage.start();
+            getMessage.start();
+            getMessage.join();
+            sendMessage.join();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void messageSend() {
+        while (true) {
+            try {
+                String message = reader.readLine();
+                if (!message.trim().isEmpty()) {
+                    if (message.equalsIgnoreCase("/exit")) System.exit(0);
+                    outputStream.writeUTF(message);
+                    System.out.println("Сообщение отправлено.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    public void messageGet() {
+        while (true) {
+            try {
+                String message = inputStream.readUTF();
+                System.out.println("Сервер написал: " + message);
+            } catch (IOException e) {
+                System.out.println("Сервер недоступен.");
+                System.exit(0);
+                break;
+            }
+        }
+    }
 }
